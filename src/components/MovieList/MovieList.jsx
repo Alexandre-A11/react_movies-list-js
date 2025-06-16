4e1b95be2625839b018012980f6d6de95a5a9b75
Alexandre Almeida <alexandredd@duck.com>
2025-06-16 20:35:01 -0300

    Remove refragment

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,13 +1,10 @@
 import './MovieList.scss';
-import { MovieCard } from '../MovieCard/MovieCard';
+import { MovieCard } from '../MovieCard';
 
 export const MovieList = ({ movies }) => (
-  <>
-    <div className="movies">
-      {movies.map(movieData => (
-        <MovieCard key={movieData.imdbId} movie={movieData} />
-      ))}
-    </div>
-    );
-  </>
+  <div className="movies">
+    {movies.map(movieData => (
+      <MovieCard key={movieData.imdbId} movie={movieData} />
+    ))}
+  </div>
 );
